4241bfff0c7415bb9b7e7a335b504aed209229b2
wangbin3 <wangbin3@joyy.com>
2021-11-04 20:51:35 +0800

    add more

--- code/src/main/java/com/six/finger/leetcode/two/MaxSubArray.java
+++ code/src/main/java/com/six/finger/leetcode/two/MaxSubArray.java
@@ -7,16 +7,29 @@ public class MaxSubArray {
     }
 
     public static  int maxSubArray(int[] nums) {
-
         int pre=0;
         int max=nums[0];
         for (int i = 0; i < nums.length-1; i++) {
             pre=Math.max(nums[i],pre+nums[i]);
-
             max=Math.max(pre,max);
-
         }
         return max;
+    }
+
+
+    public static  int maxSubArra1y(int[] nums) {
+        int res = nums[0];
+        int max = 0;
+        for (int num : nums) {
+            if (num + max > num) {
+                max = num + max;
+            } else {
+                max = num;
+            }
+            res = Math.max(max, res);
+        }
+        return res;
+
 
     }
 }
