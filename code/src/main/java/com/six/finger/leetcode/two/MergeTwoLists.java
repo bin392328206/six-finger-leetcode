96b32bde059fa3dd6247a471da3e3b1deeb1eab6
wangbin3 <wangbin3@joyy.com>
2021-11-11 19:37:20 +0800

    递归

--- code/src/main/java/com/six/finger/leetcode/two/MergeTwoLists.java
+++ code/src/main/java/com/six/finger/leetcode/two/MergeTwoLists.java
@@ -74,4 +74,26 @@ public class MergeTwoLists {
 
 
     }
+
+
+    /**
+     * @author: xiaoliuliu
+     * @Date: 2021/11/11 14:32
+     * @Description: 暴力循环解法
+     */
+    public static ListNode1 mergeTwoLists1(ListNode1 l1, ListNode1 l2) {
+
+        if (l1 == null) {
+            return l2;
+        }else
+        if (l2 == null) {
+            return l1;
+        }else if (l1.val>l2.val){
+            l2.next=mergeTwoLists(l2.next,l1);
+            return l2;
+        }else {
+            l1.next=mergeTwoLists(l1.next,l2);
+            return l1;
+        }
+    }
 }
