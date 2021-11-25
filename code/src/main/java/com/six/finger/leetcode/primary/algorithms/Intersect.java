6f8ad1d5860f4d3eafbf799b21212b97c66bb36f
wangbin3 <wangbin3@joyy.com>
2021-11-25 17:23:05 +0800

    sort

--- code/src/main/java/com/six/finger/leetcode/primary/algorithms/Intersect.java
+++ code/src/main/java/com/six/finger/leetcode/primary/algorithms/Intersect.java
@@ -15,6 +15,11 @@ public class Intersect {
 
     public static void main(String[] args) {
 
+        int[]  nums1={4,9,5};
+        int[]  nums2={9,4,9,8,4};
+        intersect1(nums1,nums2);
+
+
     }
 
     public int[] intersect(int[] nums1, int[] nums2) {
@@ -75,27 +80,36 @@ public class Intersect {
 
 
 
-    public int[] intersect1(int[] nums1, int[] nums2) {
+    public static int[] intersect1(int[] nums1, int[] nums2) {
 
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int length1 = nums1.length, length2 = nums2.length;
-        int[] intersection = new int[Math.min(length1, length2)];
-        int index1 = 0, index2 = 0, index = 0;
-        while (index1 < length1 && index2 < length2) {
-            if (nums1[index1] < nums2[index2]) {
-                index1++;
-            } else if (nums1[index1] > nums2[index2]) {
-                index2++;
-            } else {
-                intersection[index] = nums1[index1];
-                index1++;
-                index2++;
-                index++;
-            }
-        }
-        return Arrays.copyOfRange(intersection, 0, index);
 
+        int[] res=new int[nums1.length>nums2.length?nums2.length:nums1.length];
+
+        int index0=0;
+        int index1=0;
+        int index2=0;
+
+        while (index1<nums1.length &&index2<nums2.length){
+                if (nums1[index1]>nums2[index2]){
+                    index2++;
+                    continue;
+                }else if (nums1[index1]<nums2[index2]){
+                    index1++;
+                    continue;
+                }else {
+                    res[index0]=nums1[index1];
+                    index1++;
+                    index2++;
+                    index0++;
+                }
+
+        }
+
+        int[] ints = Arrays.copyOfRange(res, 0, index0);
+
+        return ints;
     }
 
 
